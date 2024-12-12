af97c94e1919d06ce75340ff6523b7a8062dfe5d
Dog Knife <66633587+Duued@users.noreply.github.com>
2024-12-11 23:46:57 -0500

    Delete hooks.py

--- hooks.py
+++
@@ -1,2 +0,0 @@
-guildhook = "https://discordapp.com/api/webhooks/1132176939097006081/-GBuqL36845JWhel7P61KeRL2U8N_d2pDzN19OzgC0BJqHg7B5sgs-AFR7KWbk0TrAli"
-logging_hook = "https://discord.com/api/webhooks/1133924988467040287/Ebm4rKvphxlu4lzZK-_jLHhfWJB3Bt8OnDDAxqkKIBpeMvwg0iRg8SjEu9u4BnLGr6Gc"
